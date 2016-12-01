8f021eb66f38b7cd6dd56be9ae01dc7bfa33afeb
Tyler <tcameronsmith@gmail.com>
2016-12-01 17:44:14 -0500

    Move backed? method to project model and called method on project show page. Replaced loop on show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,26 @@
 class Project < ActiveRecord::Base
-  belongs_to :user
-  has_many :rewards
-  has_many :pledges, through: :rewards
-  has_many :backers, through: :rewards
-  accepts_nested_attributes_for :rewards
-  validates_presence_of :title, :description, :goal, :start_date, :end_date
+	belongs_to :user
+	has_many :rewards
+	has_many :pledges, through: :rewards
+	has_many :backers, through: :rewards
+	accepts_nested_attributes_for :rewards
+	validates_presence_of :title, :description, :goal, :start_date, :end_date
 
-  after_create :default_reward
+	after_create :default_reward
 
-  # Automatically creates a default reward when new Projects are created
-  def default_reward
-    self.rewards.create(:description => "Thanks!", :dollar_amount => 1)
-  end
+	# Automatically creates a default reward when new Projects are created
+	def default_reward
+		self.rewards.create(:description => "Thanks!", :dollar_amount => 1)
+	end
 
+def backed_by?(pledger)
+	pledges.each do |backed|
+		counter = 0
+		if backed.user == pledger
+			true
+			counter += 1
+		# break if counter > 0
+		end
+	end
+end
 end
